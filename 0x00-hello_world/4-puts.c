95c03dbf7ebf9f6d1ba32ab728eb477c8b3bab4c
AbaraEmmanuel <emmanuelabara265@gmail.com>
2023-08-10 15:12:39 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+
 /**
  * main prints it out
  * followed by a new lin
  * return 0
-*/
-int main(void)
+ */
 
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return(0);

96fc1f9ef4fde5b226b5c5661000b1f884ec74fe
AbaraEmmanuel <emmanuelabara265@gmail.com>
2023-08-11 18:01:12 -0700

    Task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'Printing numbers of base 6'
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n;
+	int m;
+
+	for (m = 97; n <= 57; n++)
+	{
+		putchar(n);
+	}
+	for (m = 97; m <= 102; m++)
+	{
+		putchar(m);
+	}
+	putchar('\n');
+	return (0);
+}

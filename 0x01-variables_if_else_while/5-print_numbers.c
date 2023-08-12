8aa2a8b0b2876a8d65c4e21b10cf4c0f66e062f8
AbaraEmmanuel <emmanuelabara265@gmail.com>
2023-08-11 17:12:38 -0700

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'Pinting alphabets except q and e'
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		printf("%d", n);
+	}
+	printf("\n");
+	return (0);
+}

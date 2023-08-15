621883bb1b6d44cecad22096e4f9ca67c81495fc
AbaraEmmanuel <emmanuelabara265@gmail.com>
2023-08-15 16:09:54 -0700

    Nested loops task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include "_putchar.c"
+
+int main()
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('\n');
+	return (0);
+}

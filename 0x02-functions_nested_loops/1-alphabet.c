aca0288d690752aa14c988d163c8ca9d7fd7ac87
AbaraEmmanuel <emmanuelabara265@gmail.com>
2023-08-15 17:01:34 -0700

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_alphabet - printing in lowercase
+ */
+void print_alphabet(void)
+
+{
+
+        char let;
+
+	for (let = 'a'; let <= 'z'; let++)
+		_putchar(let);
+	_putchar('\n');
+}

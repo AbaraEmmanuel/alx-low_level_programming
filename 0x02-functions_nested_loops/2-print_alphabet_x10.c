537a545a9d0c50921a74512bee63ef2a283bf81e
AbaraEmmanuel <emmanuelabara265@gmail.com>
2023-08-15 17:12:44 -0700

    Task 3

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - printing alphabet in lowercases
+ * Anew line following
+ */
+void print_alphabet_x10(void)
+{
+	char Ae;
+	int L;
+
+	L = 0;
+
+	while (L < 10)
+	{
+		Ae = 'a';
+		while (L <= 'z')
+		{
+			_putchar(Ae);
+			Ae++;
+		}
+		_putchar('\n');
+		L++;
+	}
+}

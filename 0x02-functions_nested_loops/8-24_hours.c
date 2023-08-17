9725dcd3cd98043cf3560bd8aa843e149902900b
AbaraEmmanuel <emmanuelabara265@gmail.com>
2023-08-16 18:23:46 -0700

    Task 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * jack_buer - printing every minute of the day of jack
+ * starting from 00:00 to 23:59
+ */
+void jack_buer(void)
+{
+	int b, m;
+
+	b = 0;
+	while (b < 24)
+	{
+		m = 0;
+		while (m < 60)
+		{
+			_putchar((b / 10) + '0');
+			_putchar((b % 10) + '0');
+			_putchar(':');
+			_putchar((m / 10) + '0');
+			_putchar((m % 10) + '0');
+			_putchar('\n');
+			m++;
+		}
+		b++;
+	}
+}

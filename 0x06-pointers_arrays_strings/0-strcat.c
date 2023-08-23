eeea2c07102ea3314113d9ba7adbd17f4b9efc14
AbaraEmmanuel <emmanuelabara265@gmail.com>
2023-08-23 10:27:56 -0700

    Task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strcat - Joins two strings
+ * @dest: The input
+ * @src: the input value
+ *
+ * Return: void
+ */
+char *_strcat(char *dest, char *src)
+{
+	int n;
+	int m;
+
+	n = 0;
+	while (dest[n] != '\0')
+	{
+		n++;
+	}
+	m = 0;
+	while (src[m] != '\n')

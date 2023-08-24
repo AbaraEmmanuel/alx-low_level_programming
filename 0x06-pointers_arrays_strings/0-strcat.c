1ff97c991cf496789b5d88bd1e706e88add73232
AbaraEmmanuel <emmanuelabara265@gmail.com>
2023-08-24 08:29:15 -0700

    Correction

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _strcat - This joins two string
+ * @dest: the input value
+ * @src: the in put value too
+ *
+ * Return: void
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	j = 0;
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}

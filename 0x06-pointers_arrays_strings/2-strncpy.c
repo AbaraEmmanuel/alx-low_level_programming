5b22a9e8cd34f2420e06acc19db313a58655c500
AbaraEmmanuel <emmanuelabara265@gmail.com>
2023-08-24 17:12:36 -0700

    Task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,12 +12,12 @@ char *_strncpy(char *dest, char *src, int n)
 	int m;
 
 	m = 0;
-	while (j < n && src[m] != '\0')
+	while (m < n && src[m] != '\0')
 	{
 		dest[m] = src[m];
 		m++;
 	}
-	while (j < n)
+	while (m < n)
 	{
 		dest[m] = '\0';
 		m++;

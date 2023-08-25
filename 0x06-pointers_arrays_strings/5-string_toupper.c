1ecf2d728da27623efd8eac747e68a89a4bd72b9
AbaraEmmanuel <emmanuelabara265@gmail.com>
2023-08-24 17:46:06 -0700

    Task 4

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * string_toupper - Changing lowercase to uppercase
+ * @n: points
+ *
+ * Return: n
+ */
+char *string_toupper(char *n)
+{
+	int b;
+
+	b = 0;
+	while (n[b] != '\0')
+	{
+		if (n[b] >= 'a' && n[b] <= 'z')
+			n[b] = n[b] - 32;
+		b++;
+	}
+	return (n);
+}

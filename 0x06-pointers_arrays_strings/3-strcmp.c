49c8db228e3c66b37bf889531e575a6315e1fe75
AbaraEmmanuel <emmanuelabara265@gmail.com>
2023-08-25 04:14:10 -0700

    Task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,23 +1,23 @@
 #include "main.h"
 /**
- * _strcmp - This compares string val
- * @s1: input
- * @s2: input
+ * _strcmp - compares two string
+ * @s1: the first string
+ * @s2: the second string
  *
- * Return: s1[i] - s2[i]
+ * Return: s1[b] - s2[b]
  */
-ont _strcmp(char *s1, char *s2)
+int _strcmp(char *s1, char *s2)
 {
-	int i;
+	int b;
 
-	i = 0;
-	while (s1[i] != '\0' && s2[i] != '\0')
+	b = 0;
+	while (s1[b] != '\0' && s2[b] != '\0')
 	{
-		if (s1[i] != s2[i])
+		if (s1[b] != s2[b])
 		{
-			return (s1[i] - s2[i]);
+			return (s1[b] != s2[b]);
 		}
-		i++;
+		b++;
 	}
 	return (0);
 }

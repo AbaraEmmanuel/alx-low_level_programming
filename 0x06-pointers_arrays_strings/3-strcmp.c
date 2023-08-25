bd7224b5902f78dcf0bfb36dddceac87c6677496
AbaraEmmanuel <emmanuelabara265@gmail.com>
2023-08-24 17:31:35 -0700

    TR3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,16 +8,16 @@
  */
 ont _strcmp(char *s1, char *s2)
 {
-	int n;
+	int m;
 
-	n = 0;
-	while (s1[n] != '\0' && s2[n] != '\0')
+	m = 0;
+	while (s1[m] != '\0' && s2[m] != '\0')
 	{
-		if (s1[n] != s2[n])
+		if (s1[m] != s2[m])
 		{
-			return (s1[n] - s2[n]);
+			return (s1[m] - s2[m]);
 		}
-		n++;
+		m++;
 	}
 	return (0);
 }

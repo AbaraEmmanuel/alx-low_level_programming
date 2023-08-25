927353cb1cfe57caaf6d8138aba2be5a8cf8c36f
AbaraEmmanuel <emmanuelabara265@gmail.com>
2023-08-24 17:21:28 -0700

    Task 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strcmp - This compares string val
+ * @s1: input
+ * @s2: input
+ *
+ * Return: s1[n] - s2[n]
+ */
+ont _strcmp(char *s1, char *s2)
+{
+	int n;
+
+	n = 0;
+	while (s1[n] != '\0' && s2[n] != '\0')
+	{
+		if (s1[n] != s2[n])
+		{
+			return (s[n] - s2[n]);
+		}
+		n++;
+	}
+	return (0);
+}

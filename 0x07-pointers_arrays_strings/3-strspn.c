eadb716d50085233dce25c53570c77d57145b389
AbaraEmmanuel <emmanuelabara265@gmail.com>
2023-08-28 18:42:18 -0700

    Task 3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _strspn - Entry
+ * @s: inp
+ * @accept: inp
+ * Return: Always 0.
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int h = 0;
+	int b;
+
+	while (*s)
+	{
+		for (b = 0; accept[b]; b++)
+		{
+			if (*s == accept[b])
+			{
+				h++;
+				break;
+			}
+			else if (accept[b + 1] == '\0')
+				return (n);
+		}
+		s++;
+	}
+	return (n);
+}

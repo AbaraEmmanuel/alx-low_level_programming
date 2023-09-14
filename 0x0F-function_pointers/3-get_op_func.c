2774d22b2f298d25d06f17f43371ce56d6db243a
AbaraEmmanuel <emmanuelabara265@gmail.com>
2023-09-13 17:53:11 -0700

    TASK 3 correction

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -2,13 +2,13 @@
 #include <stdlib.h>
 /**
  * get_op_func - Selects the correct function to perform
- * @s: The operator passed as arg
- * Return: A pointer to the function corresponding
+ * @s: The operator passed as argument
+ *
+ *  Return: A pointer to the function
  */
 int (*get_op_func(char *s))(int, int)
 {
-	op_t ops[] = 
-	{
+	op_t ops[] = {
 		{"+", op_add},
 		{"-", op_sub},
 		{"*", op_mul},

bb3dfaac95feba31e495096a65f252c778c0e124
AbaraEmmanuel <emmanuelabara265@gmail.com>
2023-10-02 11:44:34 -0700

    Task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,33 +1,47 @@
-#include "holberton.h"
-
+#include "main.h"
 /**
- * create_file - create a file with read/write access for user
- * @filename: name of file to create
- * @text_content: string to write to file
- * Return: 1 on success, -1 on failure
- */
+  * _strlen - length of a string
+  * @s: input char
+  * Return: length of a string
+**/
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+	{
+		i++;
+	}
+	return (i);
+}
+/**
+* create_file - check the code for Holberton School students.
+* @filename: file to create.
+* @text_content: info to write into the file.
+* Return: 1 on success, -1 on failure
+*/
 int create_file(const char *filename, char *text_content)
 {
-int fd, rstatus, i;
+	ssize_t nletters;
+	int file;
 
-if (filename == NULL)
-	return (-1);
-
-fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
-
-if (fd == -1)
-	return (-1);
-
-if (text_content)
-{
-	for (i = 0; text_content[i] != '\0'; i++)
-		;
-	rstatus = write(fd, text_content, i);
-	if (rstatus == -1)
+	if (!filename)
+		return (1);
+	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (file == -1)
+	{
 		return (-1);
-}
-
-close(fd);
-return (1);
+	}
+	if (text_content)
+	{
+		nletters = write(file, text_content, _strlen(text_content));
+		if (nletters == -1)
+		{
+			close(file);
+			return (-1);
+		}
+	}
+	close(file);
+	return (1);
 }
 

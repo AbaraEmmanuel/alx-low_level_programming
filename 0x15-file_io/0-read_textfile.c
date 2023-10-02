4f938aeb6fdb689251d03c8c66e461425333a9be
AbaraEmmanuel <emmanuelabara265@gmail.com>
2023-10-02 11:16:33 -0700

    task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,48 +1,42 @@
 #include "main.h"
-
 /**
-* read_textfile - Read a text file and write it to STDOUT.
-* @filename: The name of the file to read.
-* @letters: The number of letters to read and write.
-* Return: The number of letters actually read and written, or 0 on failure.
+* read_textfile - check the code for Holberton School students.
+* @filename: file to read and write
+* @letters: number of letters to read and write.
+* Return: letters printed
 */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    ssize_t nletters;
-    int file;
-    char *text;
+	ssize_t nletters;
+	int file;
+	char *text;
 
-    if (!filename)
-        return (0);
-
-    text = malloc(sizeof(char) * letters);
-    if (text == NULL)
-        return (0);
-
-    file = open(filename, O_RDONLY);
-    if (file == -1)
-    {
-        free(text);
-        return (0);
-    }
-
-    nletters = read(file, text, letters);
-    if (nletters == -1)
-    {
-        free(text);
-        close(file);
-        return (0);
-    }
-
-    nletters = write(STDOUT_FILENO, text, nletters);
-    if (nletters == -1)
-    {
-        free(text);
-        close(file);
-        return (0);
-    }
-
-    free(text);
-    close(file);
-    return (nletters);
-}
+	if (!filename)
+		return (0);
+	text = malloc(sizeof(char) * letters + 1);
+	if (text == NULL)
+		return (0);
+	file = open(filename, O_RDONLY);
+	if (file == -1)
+	{
+		free(text);
+		return (0);
+	}
+	nletters = read(file, text, sizeof(char) * letters);
+	if (nletters == -1)
+	{
+		free(text);
+		close(file);
+		return (0);
+	}
+	nletters = write(STDOUT_FILENO, text, nletters);
+	if (nletters == -1)
+	{
+		free(text);
+		close(file);
+		return (0);
+	}
+	free(text);
+	close(file);
+	return (nletters);
+}}
